527127e5575bb15c664222193cb8065857dc9fa4
timo-do <timodo@live.de>
2023-02-05 20:52:58 +0100

    wip building process control

--- bin/deploy/app.py
+++ bin/deploy/app.py
@@ -1,6 +1,7 @@
 #!/home/pi/venvs/photobox/bin/python
 import flask
 import networking
+import housekeeping
 
 app = flask.Flask(__name__, template_folder="www/templateFiles", static_folder="www/staticFiles")
 
@@ -10,6 +11,9 @@ def index():
         {
             "title"     :   "Befehle",
             "target"    :   "commands" 
+        },{
+            "title"     :   "Prozesse",
+            "target"    :   "procs" 
         }
     ]
     return flask.render_template("navigation_rows.html", navs=navs)
@@ -27,12 +31,57 @@ def commands():
             "subtitle"  : "Schieße ein Photo.",
             "cmd"       : "shutter"
         },
+        {
+            "title"     : "Bye!",
+            "subtitle"  : "Fährt das System herunter.",
+            "cmd"       : "shutdown"
+        }
     ]
     return flask.render_template("command_tiles.html", commands = commands, ret="index")
+
+@app.route("/procs")
+def procs():
+    state_colors = {
+        "FATAL"         : "red",
+        "RUNNING"       : "green",
+        "RESTARTING"    : "orange"
+    }
+    procs = housekeeping.supervisor_get_process_info()
+    elems = []
+    for proc in procs:
+        elems.append({
+            "name"      :   proc["name"],
+            "state"     :   proc["statename"],
+            "color"     :   state_colors.get(proc["statename"], "black"),
+            "route"     :   "proc_info",
+        })
+    return flask.render_template("process_rows.html", elems=elems, ret="index")
+
+@app.route("/proc_info/<proc>")
+def proc_info(proc = None):
+    state_colors = {
+        "FATAL"         : "red",
+        "RUNNING"       : "green",
+        "RESTARTING"    : "orange"
+    }
+    procs = housekeeping.supervisor_get_process_info(proc)
+    elems = [{
+            "name"      :   "Test",
+            "state"     :   "Test",
+            "color"     :   "red",
+            "route"     :   "index"
+        }]
+
+    return flask.render_template("process_infos.html", elems=elems, ret="index")
 
 @app.route("/toggle")
 def toggle():
     networking.command("TOGGLESCREEN")
+    return ""
+
+@app.route("/shutdown")
+def shutdown():
+    networking.command("SHUTDOWN")
     return ""
 
 
