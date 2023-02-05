f3534be01be17c6a3e9489c2aa79abb0be98bde3
timo-do <timodo@live.de>
2023-02-05 20:51:48 +0100

    init

---
+++ bin/deploy/housekeeping.py
@@ -0,0 +1,34 @@
+#!/home/pi/venvs/photobox/bin/python
+import networking
+import subprocess
+import os
+import logging
+import assets.tools
+import time
+from xmlrpc.client import ServerProxy
+
+logger = assets.tools.get_logger("HOUSEKEEPING")
+server = ServerProxy("http://localhost:9001/RPC2")
+
+def run_bash(cmd):
+    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    return output.decode("ascii")
+
+def shutdown():
+    #run_bash("sudo shutdown now")
+    logger.error("Shutdown deactivated ... for now")
+
+def supervisor_get_process_info(proc = None):
+    if(proc is None):
+        return server.supervisor.getAllProcessInfo()
+    else:
+        return server.supervisor.getProcessInfo(proc)
+
+def supervsior_get_log(name):
+    return server.supervisor.readProcessStderrLog(name, 0, 0)
+
+if __name__ == "__main__":
+    networking.on_command("SHUTDOWN", shutdown)
+    print(supervsior_get_log("flask"))
+
